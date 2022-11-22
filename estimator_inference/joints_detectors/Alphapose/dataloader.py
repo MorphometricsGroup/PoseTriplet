231a911ee8ec574d81f0a262fddaf57347543d41
colum2131 <kohei.i.2131@gmail.com>
2022-11-22 13:01:26 +0900

    change: relative paths

--- estimator_inference/joints_detectors/Alphapose/dataloader.py
+++ estimator_inference/joints_detectors/Alphapose/dataloader.py
@@ -271,8 +271,8 @@ class DetectionLoader:
     def __init__(self, dataloder, batchSize=1, queueSize=1024):
         # initialize the file video stream along with the boolean
         # used to indicate if the thread should be stopped or not
-        self.det_model = Darknet("joints_detectors/Alphapose/yolo/cfg/yolov3-spp.cfg")
-        self.det_model.load_weights('joints_detectors/Alphapose/models/yolo/yolov3-spp.weights')
+        self.det_model = Darknet("PoseTriplet/estimator_inference/joints_detectors/Alphapose/yolo/cfg/yolov3-spp.cfg")
+        self.det_model.load_weights('PoseTriplet/estimator_inference/joints_detectors/Alphapose/models/yolo/yolov3-spp.weights')
         self.det_model.net_info['height'] = opt.inp_dim
         self.det_inp_dim = int(self.det_model.net_info['height'])
         assert self.det_inp_dim % 32 == 0
@@ -429,8 +429,8 @@ class VideoDetectionLoader:
     def __init__(self, path, batchSize=4, queueSize=256):
         # initialize the file video stream along with the boolean
         # used to indicate if the thread should be stopped or not
-        self.det_model = Darknet("yolo/cfg/yolov3-spp.cfg")
-        self.det_model.load_weights('models/yolo/yolov3-spp.weights')
+        self.det_model = Darknet("PoseTriplet/estimator_inference/joints_detectors/Alphapose/yolo/cfg/yolov3-spp.cfg")
+        self.det_model.load_weights('PoseTriplet/estimator_inference/joints_detectors/Alphapose/models/yolo/yolov3-spp.weights')
         self.det_model.net_info['height'] = opt.inp_dim
         self.det_inp_dim = int(self.det_model.net_info['height'])
         assert self.det_inp_dim % 32 == 0
